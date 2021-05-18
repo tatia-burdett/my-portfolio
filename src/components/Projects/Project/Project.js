5c833de612f6bf9da44ffa71962bb77bb38a9f0d
Tatia burdett <tatiaburdett@gmail.com>
2021-05-18 12:46:43 -0700

    add classname

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Project extends React.Component {
   render() {
     return (
-      <div>
+      <div className='project'>
         <h4>Single Project</h4>
       </div>
     )
