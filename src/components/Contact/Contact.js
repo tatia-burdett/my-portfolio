205345b7bc6d1c6c8f06d6cd0fcd22e0a8469110
Tatia burdett <tatiaburdett@gmail.com>
2021-05-18 13:52:55 -0700

    contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,21 @@ class Contact extends React.Component {
   render() {
     return (
       <section className='contact'>
-        <h3>Contact Me</h3>
+        <h3>Contact</h3>
+        <div className='contact-section'>
+          <form action="https://formspree.io/f/xzbkaken " method="POST">
+            <label htmlFor="user-name">Name:</label>
+            <input id="user-name" type="text" name="name" placeholder="Enter your name" />
+
+            <label htmlFor="user-email">Email:</label>
+            <input id="user-email" type="text" name="email" placeholder="Enter your email" />
+
+            <label htmlFor="user-message">Message:</label>
+            <textarea id="user-message" name="message" placeholder="Enter a short message" />
+
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </section>
     )
   }
