3cf3628078998366ebad0881f1425980922b6e4b
Tatia burdett <tatiaburdett@gmail.com>
2021-05-18 13:28:50 -0700

    add icons

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,18 +1,26 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHtml5, faCss3, faJs, faReact, faNodeJs, faGitSquare } from '@fortawesome/free-brands-svg-icons'
+import './Skills.css'
+
+{/* <i class="fab fa-git-square"></i> */}
 
 class Skills extends React.Component {
   render() {
     return (
       <section className='skills'>
         <h3>Skills</h3>
-        <p>HTML</p>
-        <p>CSS</p>
-        <p>JavaScript</p>
-        <p>jQuery</p>
-        <p>React</p>
-        <p>Node.js</p>
-        <p>Express</p>
-        <p>PostgreSQL</p>
+        <div className='skills-container'>
+          <FontAwesomeIcon icon={faHtml5} className='skill-icon'/>
+          <FontAwesomeIcon icon={faCss3} className='skill-icon'/>
+          <FontAwesomeIcon icon={faJs} className='skill-icon'/>
+          <FontAwesomeIcon icon={faReact} className='skill-icon'/>
+          <FontAwesomeIcon icon={faNodeJs} className='skill-icon'/>
+          <FontAwesomeIcon icon={faGitSquare} className='skill-icon'/>
+          <img src="https://img.icons8.com/ios-filled/50/000000/jquery.png" alt=''/>
+          <img src="https://img.icons8.com/ios-filled/50/000000/postgreesql.png" alt=''/>
+          <img src="https://img.icons8.com/ios-filled/50/000000/sql.png" alt=''/>
+        </div>
       </section>
     )
   }
