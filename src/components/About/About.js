fea0f9ff2fae6e4ea86de9274816c3596a41cf36
Tatia burdett <tatiaburdett@gmail.com>
2021-05-18 12:46:18 -0700

    about me page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,11 @@ import React from 'react'
 class About extends React.Component {
   render() {
     return (
-      <div>About</div>
+      <section className='about'>
+        <h3>About Me</h3>
+        <p>Text about me... </p>
+        <p>Possibly an image as well</p>
+      </section>
     )
   }
 }
