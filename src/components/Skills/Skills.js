ff405ea7b1d0781ffd030744d3f0b14ec43ea727
Tatia burdett <tatiaburdett@gmail.com>
2021-05-18 12:42:29 -0700

    add skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -3,7 +3,16 @@ import React from 'react'
 class Skills extends React.Component {
   render() {
     return (
-      <div>Skills</div>
+      <section>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>JavaScript</p>
+        <p>jQuery</p>
+        <p>React</p>
+        <p>Node.js</p>
+        <p>Express</p>
+        <p>PostgreSQL</p>
+      </section>
     )
   }
 }
