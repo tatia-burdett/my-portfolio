71aff8e015e1dc6da6ea32f8a1cc6b5659bef4b4
Tatia burdett <tatiaburdett@gmail.com>
2021-05-18 13:56:31 -0700

    footer text and icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import './Footer.css'
+
+{/* <i class="fab fa-linkedin-in"></i> */}
 
 class Footer extends React.Component {
   render() {
     return (
       <div className='footer'>
-        <p>Add links to external sites, other personal sites (blog, photography...)</p>
-        <p>Links for LinkedIn / GitHub</p>
+        <p>Created by: Tatia Burdett</p>
+        <FontAwesomeIcon icon={faGithub} className='footer-icon'/>
+        <FontAwesomeIcon icon={faLinkedinIn} className='footer-icon'/>
       </div>
     )
   }
