ad3b334fc6d7df3c330f9037c25f6db8fa5565e3
Tatia burdett <tatiaburdett@gmail.com>
2021-05-21 18:06:27 -0700

    grid skills, full screen padding, responsive

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -9,15 +9,42 @@ class Skills extends React.Component {
       <section className='skills' id='skills'>
         <h3>Skills</h3>
         <div className='skills-container'>
-          <FontAwesomeIcon icon={faHtml5} className='skill-icon'/>
-          <FontAwesomeIcon icon={faCss3} className='skill-icon'/>
-          <FontAwesomeIcon icon={faJs} className='skill-icon'/>
-          <FontAwesomeIcon icon={faReact} className='skill-icon'/>
-          <FontAwesomeIcon icon={faNodeJs} className='skill-icon'/>
-          <FontAwesomeIcon icon={faGitSquare} className='skill-icon'/>
-          <img src="https://img.icons8.com/ios-filled/50/000000/jquery.png" alt=''/>
-          <img src="https://img.icons8.com/ios-filled/50/000000/postgreesql.png" alt=''/>
-          <img src="https://img.icons8.com/ios-filled/50/000000/sql.png" alt=''/>
+          <div>
+            <FontAwesomeIcon icon={faHtml5} className='skill-icon'/>
+            <p>HTML</p>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faCss3} className='skill-icon'/>
+            <p>CSS</p>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faJs} className='skill-icon'/>
+            <p>JavaScript</p>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faReact} className='skill-icon'/>
+            <p>React</p>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faNodeJs} className='skill-icon'/>
+            <p>Node.js</p>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faGitSquare} className='skill-icon'/>
+            <p>Git</p>
+          </div>
+          <div>
+            <img src="https://img.icons8.com/ios-filled/50/000000/jquery.png" alt=''/>
+            <p>jQuery</p>
+          </div>
+          <div>
+            <img src="https://img.icons8.com/ios-filled/50/000000/postgreesql.png" alt=''/>
+            <p>Postgres</p>
+          </div>
+          <div>
+            <img src="https://img.icons8.com/ios-filled/50/000000/sql.png" alt=''/>   
+            <p>SQL</p>         
+          </div>
         </div>
       </section>
     )
