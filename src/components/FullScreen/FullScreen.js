67e68875d8fc3d4aa96d9aed859ec33307f1737e
Tatia burdett <tatiaburdett@gmail.com>
2021-05-25 15:16:04 -0700

    remove previous styling

--- src/components/FullScreen/FullScreen.js
+++ src/components/FullScreen/FullScreen.js
@@ -5,8 +5,9 @@ class FullScreen extends React.Component {
   render() {
     return (
       <header className='fullscreen' id='fullscreen'>
+        <p>Hello, my name is</p>
         <h1>Tatia Burdett</h1>
-        <h2>Full Stack Web Developer</h2>
+        <h2>I am a Full Stack Web Developer</h2>
       </header>
     )
   }
