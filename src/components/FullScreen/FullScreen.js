ea77d05b8192c9c88da9e15e1bf2116ffa1b267c
Tatia burdett <tatiaburdett@gmail.com>
2021-05-25 21:21:04 -0700

    header positioning and size

--- src/components/FullScreen/FullScreen.js
+++ src/components/FullScreen/FullScreen.js
@@ -6,10 +6,10 @@ class FullScreen extends React.Component {
     return (
       <header className='fullscreen' id='fullscreen'>
         <div className='fullscreen-header'>
-          <p>Hello, my name is</p>
+          <p className='header-p'>Hello, my name is</p>
           <h1>Tatia Burdett</h1>
           <h2>I am a Full Stack Web Developer</h2>
-          <p>Currently open to opportunities throughout the US.</p>
+          <p className='header-p'>Currently open to opportunities throughout the US.</p>
           <a href='https://drive.google.com/file/d/1xGdbrtnpaXpPFPEieWNSfUfGjhnRkwe9/view?usp=sharing' target='_blank' rel='noreferrer' className='resume'>Resume</a>
         </div>
       </header>
