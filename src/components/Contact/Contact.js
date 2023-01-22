ff281df6d6272d7ee160432e2365662ed38ed392
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-22 10:29:40 -0600

    update contact to functional comp

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,28 +1,24 @@
 import React from 'react'
 import './Contact.css'
 
-class Contact extends React.Component {
-  render() {
-    return (
-      <section className='contact' id='contact'>
-        <h3>Contact Me</h3>
-        <div className='contact-section'>
-          <form action="https://formspree.io/f/xzbkaken " method="POST">
-            <label htmlFor="user-name">Name:</label>
-            <input id="user-name" type="text" name="name" placeholder="Enter your name" />
+export default function Contact() {
+  return (
+    <section className='contact' id='contact'>
+      <h3>Contact Me</h3>
+      <div className='contact-section'>
+        <form action="https://formspree.io/f/xzbkaken " method="POST">
+          <label htmlFor="user-name">Name:</label>
+          <input id="user-name" type="text" name="name" placeholder="Enter your name" />
 
-            <label htmlFor="user-email">Email:</label>
-            <input id="user-email" type="text" name="email" placeholder="Enter your email" />
+          <label htmlFor="user-email">Email:</label>
+          <input id="user-email" type="text" name="email" placeholder="Enter your email" />
 
-            <label htmlFor="user-message">Message:</label>
-            <textarea id="user-message" name="message" placeholder="Enter a short message" />
+          <label htmlFor="user-message">Message:</label>
+          <textarea id="user-message" name="message" placeholder="Enter a short message" />
 
-            <button type="submit">Submit</button>
-          </form>
-        </div>
-      </section>
-    )
-  }
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </section> 
+  )
 }
-
-export default Contact
