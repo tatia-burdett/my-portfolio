2baf44720001b0992d6d89b8963988b6e2099437
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-22 10:38:00 -0600

    update to functional comp

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,20 +2,16 @@ import React from 'react'
 import './NavBar.css'
 
 
-class NavBar extends React.Component {
-  render() {
-    return (
-      <div className='navbar'>
-        <a href='#fullscreen' className='logo'>{'<TB/>'}</a>
-        <ul className='navbar-links'>
-          <li><a href='#about'>About</a></li>
-          <li><a href='#skills'>Skills</a></li>
-          <li><a href='#projects'>Projects</a></li>
-          <li><a href='#contact'>Contact</a></li>
-        </ul>
-      </div>
-    )
-  }
+export default function NavBar() {
+  return (
+    <div className='navbar'>
+      <a href='#fullscreen' className='logo'>{'<TB/>'}</a>
+      <ul className='navbar-links'>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#skills'>Skills</a></li>
+        <li><a href='#projects'>Projects</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+    </div>
+  )
 }
-
-export default NavBar
