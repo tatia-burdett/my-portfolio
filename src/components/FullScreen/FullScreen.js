780612a538f6eba6ed66d7f7a3afadb5a0ef18a7
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-22 10:37:20 -0600

    update to functional comp

--- src/components/FullScreen/FullScreen.js
+++ src/components/FullScreen/FullScreen.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import './FullScreen.css'
 
-class FullScreen extends React.Component {
-  render() {
-    return (
-      <header className='fullscreen' id='fullscreen'>
-        <div className='fullscreen-header'>
-          <p className='header-p'>Hello, my name is</p>
-          <h1>Tatia Burdett</h1>
-          <h2>I am a Full Stack Web Developer</h2>
-          <p className='header-p'>Currently open to opportunities throughout the US.</p>
-          <a href='https://drive.google.com/file/d/1xGdbrtnpaXpPFPEieWNSfUfGjhnRkwe9/view?usp=sharing' target='_blank' rel='noreferrer' className='resume'>Resume</a>
-        </div>
-      </header>
-    )
-  }
+export default function FullScreen() {
+  return (
+    <header className='fullscreen' id='fullscreen'>
+      <div className='fullscreen-header'>
+        <p className='header-p'>Hello, my name is</p>
+        <h1>Tatia Burdett</h1>
+        <h2>I am a Full Stack Web Developer</h2>
+        <p className='header-p'>Currently open to opportunities throughout the US.</p>
+        <a href='https://drive.google.com/file/d/1xGdbrtnpaXpPFPEieWNSfUfGjhnRkwe9/view?usp=sharing' target='_blank' rel='noreferrer' className='resume'>Resume</a>
+      </div>
+    </header>
+  )
 }
-
-export default FullScreen
