3a45fedb3ad3e991921cad82a5055364476e351b
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-22 10:31:24 -0600

    update footer to functional comp
    `

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,22 +3,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 import './Footer.css'
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <div className='footer'>
-        <p>&#169; Tatia Burdett</p>
-        <div>
-          <a href='https://github.com/tatia-burdett' target='_blank' rel='noreferrer' className='social-links'>
-            <FontAwesomeIcon icon={faGithub} className='footer-icon'/>
-          </a>
-          <a href='https://www.linkedin.com/in/tatjanaburdett/' target='_blank' rel='noreferrer' className='social-links'>
-            <FontAwesomeIcon icon={faLinkedinIn} className='footer-icon'/>
-          </a>
-        </div>
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>&#169; Tatia Burdett</p>
+      <div>
+        <a href='https://github.com/tatia-burdett' target='_blank' rel='noreferrer' className='social-links'>
+          <FontAwesomeIcon icon={faGithub} className='footer-icon'/>
+        </a>
+        <a href='https://www.linkedin.com/in/tatjanaburdett/' target='_blank' rel='noreferrer' className='social-links'>
+          <FontAwesomeIcon icon={faLinkedinIn} className='footer-icon'/>
+        </a>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Footer
