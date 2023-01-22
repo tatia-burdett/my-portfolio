257859fa55f5c6e81e72b76668974bfc20213e3d
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-22 15:56:35 -0600

    form bootstrap

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './Contact.css'
 
 export default function Contact() {
@@ -6,18 +8,18 @@ export default function Contact() {
     <section className='contact' id='contact'>
       <h3>Contact Me</h3>
       <div className='contact-section'>
-        <form action="https://formspree.io/f/xzbkaken " method="POST">
-          <label htmlFor="user-name">Name:</label>
-          <input id="user-name" type="text" name="name" placeholder="Enter your name" />
+        <Form action="https://formspree.io/f/xzbkaken " method="POST">
+          <Form.Label>Name:</Form.Label>
+          <Form.Control type="text" placeholder="Name" />
 
-          <label htmlFor="user-email">Email:</label>
-          <input id="user-email" type="text" name="email" placeholder="Enter your email" />
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
 
-          <label htmlFor="user-message">Message:</label>
-          <textarea id="user-message" name="message" placeholder="Enter a short message" />
-
-          <button type="submit">Submit</button>
-        </form>
+          <Form.Label>Message</Form.Label>
+          <Form.Control type="textarea" placeholder="Message" />
+          
+          <Button variant='dark' type='submit'>Submit</Button>
+        </Form>
       </div>
     </section> 
   )
