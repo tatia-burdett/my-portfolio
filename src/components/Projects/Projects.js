c606dec6ed32f7f831d0aefca965d3d0ff88bcb2
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-22 10:38:41 -0600

    update to functional comp

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,15 +3,11 @@ import './Projects.css'
 
 import Project from './Project/Project'
 
-class Projects extends React.Component {
-  render() {
-    return (
-      <section className='projects' id='projects'>
-        <h3>Solo Projects</h3>
-        <Project />
-      </section>
-    )
-  }
+export default function Projects() {
+  return (
+    <section className='projects' id='projects'>
+      <h3>Solo Projects</h3>
+      <Project />
+    </section>
+  )
 }
-
-export default Projects
