5b1122f91bea2ba819fb47a8e11493744b77a96b
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-22 11:30:01 -0600

    update fullscreen section, rm resume

--- src/components/FullScreen/FullScreen.js
+++ src/components/FullScreen/FullScreen.js
@@ -8,8 +8,7 @@ export default function FullScreen() {
         <p className='header-p'>Hello, my name is</p>
         <h1>Tatia Burdett</h1>
         <h2>I am a Full Stack Web Developer</h2>
-        <p className='header-p'>Currently open to opportunities throughout the US.</p>
-        <a href='https://drive.google.com/file/d/1xGdbrtnpaXpPFPEieWNSfUfGjhnRkwe9/view?usp=sharing' target='_blank' rel='noreferrer' className='resume'>Resume</a>
+        <p className='header-p'>Currently open to opportunities throughout the US and Europe.</p>
       </div>
     </header>
   )
