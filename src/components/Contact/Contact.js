be4a15333c1d5704ba98868dc35658d5a5baf97b
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-23 09:27:25 -0600

    update contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 import './Contact.css'
 
 export default function Contact() {
   return (
     <section className='contact' id='contact'>
       <h3>Contact Me</h3>
-      <div className='contact-section'>
+      <Container className='contact-section'>
         <Form action="https://formspree.io/f/xzbkaken " method="POST">
           <Form.Label>Name:</Form.Label>
           <Form.Control type="text" placeholder="Name" />
@@ -16,11 +17,11 @@ export default function Contact() {
           <Form.Control type="email" placeholder="Enter email" />
 
           <Form.Label>Message</Form.Label>
-          <Form.Control type="textarea" placeholder="Message" />
-          
+          <Form.Control as="textarea" rows={3} />
+
           <Button variant='dark' type='submit'>Submit</Button>
         </Form>
-      </div>
+      </Container>
     </section> 
   )
 }
