98cf8618cf82aaa19813380533d55eaa5436300b
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-24 14:04:30 -0600

    navbar bg

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,8 +8,8 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function NavBar() {
   return (
-    <Navbar className="navbar-color" bg="dark" variant="dark">
-      <Container className="navbar-container">
+    <Navbar variant="dark">
+      <Container>
         <Nav>
           <Navbar.Brand href='#fullscreen'>TB</Navbar.Brand>
           <Nav.Link href='#about'>About</Nav.Link>
