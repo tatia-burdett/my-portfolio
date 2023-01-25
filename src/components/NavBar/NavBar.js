1d5d117a823430df9fc212bf6afaac9d5538503c
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-25 15:46:03 -0600

    navbar styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -30,17 +30,3 @@ export default function NavBar() {
     </Navbar>
   )
 }
-
-// export default function NavBar() {
-//   return (
-//     <div className='navbar'>
-//       <a href='#fullscreen' className='logo'>{'<TB/>'}</a>
-//       <ul className='navbar-links'>
-//         <li><a href='#about'>About</a></li>
-//         <li><a href='#skills'>Skills</a></li>
-//         <li><a href='#projects'>Projects</a></li>
-//         <li><a href='#contact'>Contact</a></li>
-//       </ul>
-//     </div>
-//   )
-// }
