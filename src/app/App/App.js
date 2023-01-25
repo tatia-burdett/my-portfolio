98998a9add20a953268c27f81225eeba5a48f939
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-25 16:11:29 -0600

    rm files

--- src/app/App/App.js
+++ src/app/App/App.js
@@ -6,9 +6,6 @@ import Col from 'react-bootstrap/Col';
 
 import NavBar from '../../components/NavBar/NavBar'
 import Home from '../../pages/Home/Home'
-import About from '../../components/About/About'
-import Contact from '../../components/Contact/Contact'
-import Footer from '../../components/Footer/Footer'
 
 
 function App() {
@@ -25,11 +22,6 @@ function App() {
         <Row>
           <Col>
             <Home />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <About />
           </Col>
         </Row>
       </Container>
