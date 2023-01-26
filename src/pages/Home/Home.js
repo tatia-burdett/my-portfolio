c30b093c2838bf7fd7af0df76e84d4d9ed558664
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-25 18:03:57 -0600

    typewriter

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import Typewriter from 'typewriter-effect';
 
 export default function Home() {
   return (
@@ -7,8 +8,16 @@ export default function Home() {
       <div className='fullscreen-header'>
         <p className='header-p'>Hello, my name is</p>
         <h1>Tatia Burdett</h1>
-        <h2>I am a Full Stack Web Developer</h2>
-        <p className='header-p'>Currently open to opportunities throughout the US and Europe.</p>
+        <h2> I am a:
+          <Typewriter
+          options={{
+            strings: ['full stack web developer', 'musician', 'photographer'],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 10,
+          }}
+        />
+        </h2>
       </div>
     </header>
   )
