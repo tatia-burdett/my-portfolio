a87c4e14b198b1921cc3a73664802b07fbb1f3e4
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-28 12:50:09 -0600

    lorem ipsum

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -1,7 +1,49 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function About() {
-  return(<div>About page</div>);
+  return (
+    <Container>
+      <h1>Learn About Me</h1>
+      <Row>
+        <Col>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus
+          at enim id porta. Mauris vehicula magna id nisl elementum, nec
+          tincidunt orci rhoncus. Vivamus eu dictum est. Etiam tristique est sit
+          amet pretium pellentesque. Vestibulum lobortis pulvinar metus. Aenean
+          eget lacus lacus. Vestibulum venenatis arcu mi, sit amet luctus tortor
+          sodales eget. Sed varius mi at auctor hendrerit. Maecenas felis
+          turpis, hendrerit non ipsum quis, accumsan porttitor lorem. Donec
+          ornare lectus quis justo maximus ultricies. Proin enim nunc, sodales
+          eu accumsan a, dapibus at tellus. Mauris ipsum eros, iaculis at elit
+          non, porttitor elementum nibh. Donec cursus id sem vel malesuada.
+          Aliquam nec libero in nunc sollicitudin posuere at vel elit. Aenean
+          sed ante sed libero ultricies lobortis. In pellentesque rhoncus
+          sagittis. Proin ornare tempus mauris, eu volutpat tellus. Sed nibh
+          diam, tristique et tellus non, sodales gravida ligula. Integer
+          volutpat odio nec ante vulputate, et pharetra massa hendrerit. Morbi
+          at quam nulla. Nunc quis enim eget lacus dictum faucibus sit amet nec
+          ligula. Etiam pretium pretium convallis. Sed scelerisque in sem quis
+          venenatis. Ut efficitur congue eros, blandit maximus justo aliquet
+          eget. Nunc metus risus, sollicitudin non blandit ac, faucibus in
+          felis. Maecenas eu convallis dui. Proin luctus dolor hendrerit,
+          dignissim nibh quis, rhoncus ante. Ut velit felis, venenatis sed
+          libero vitae, finibus vestibulum leo.
+        </Col>
+        <Col sm={4}>
+          Nunc rhoncus nulla vitae ante mattis, posuere sagittis arcu luctus. Ut
+          nec euismod ipsum. Aenean pellentesque at neque non iaculis. Nam
+          semper metus eget dolor ullamcorper, vel varius dui mollis. Vivamus
+          vitae vestibulum dui. In tempus pellentesque ullamcorper. Duis magna
+          felis, rutrum nec aliquam quis, molestie ac massa. Etiam hendrerit,
+          justo nec pellentesque vulputate, ante dui blandit justo, nec commodo
+          tellus mi sit amet nisl.
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default About;
