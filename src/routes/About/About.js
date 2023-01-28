7ce0749a0466ee43fd6afa2b73f491b4cdbe64a3
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-28 13:13:56 -0600

    button

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -2,8 +2,9 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import img from '../../images/profile1.jpeg'
-import Image from 'react-bootstrap/Image'
+import img from "../../images/profile1.jpeg";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
 
 function About() {
   return (
@@ -11,21 +12,26 @@ function About() {
       <h1>Learn About Me</h1>
       <Row>
         <Col>
-          Well rounded experience transitioning from a successful humanities
-          profession in nursing to software engineering. Working in healthcare
-          for half of a decade taught me the importance of paying attention to
-          detail and helped to develop an agile mentality of solutioning in a
-          scalable and repeatable manner. After deciding to pursue my software
-          engineering career full-time, I have taken my “lessons-learned” from
-          my previous experience and applied my critical thinking prowess into
-          positively impacting what I saw firsthand as an area needing
-          improvement in an important industry. As I’ve continued expanding my
-          expertise while leveraging my previous experience, I have found my
-          current passion aligning with software engineering holistically and am
-          dedicating myself to growing in that expertise specifically.
+          <Row>
+            Well rounded experience transitioning from a successful humanities
+            profession in nursing to software engineering. Working in healthcare
+            for half of a decade taught me the importance of paying attention to
+            detail and helped to develop an agile mentality of solutioning in a
+            scalable and repeatable manner. After deciding to pursue my software
+            engineering career full-time, I have taken my “lessons-learned” from
+            my previous experience and applied my critical thinking prowess into
+            positively impacting what I saw firsthand as an area needing
+            improvement in an important industry. As I’ve continued expanding my
+            expertise while leveraging my previous experience, I have found my
+            current passion aligning with software engineering holistically and
+            am dedicating myself to growing in that expertise specifically.
+          </Row>
+          <Row sm={6}> 
+            <Button>Resume</Button>
+          </Row>
         </Col>
         <Col sm={4}>
-          <Image src={img} fluid rounded={true}/>
+          <Image src={img} fluid rounded={true} />
         </Col>
       </Row>
     </Container>
