e84332df909c90b21e462bddcb246fa737e0dbfc
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-29 14:25:59 -0600

    style

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.css";
 import Typewriter from "typewriter-effect";
 import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 
 export default function Home() {
   return (
@@ -11,27 +12,29 @@ export default function Home() {
       justifyContent="center"
       alignItems="center"
       height='100vh'
-    >
+    > 
+    <Container sx={{ borderRight: 1, borderTop: 1, p: 10 }}>
       <p className="header-p">Hello, my name is</p>
-      <h1>Tatia Burdett</h1>
-      <h2>
-        <Typewriter
-          options={{
-            strings: [
-              "Full Stack Web Developer",
-              "Musician",
-              "Photographer",
-              "Traveler",
-              "Mother",
-              "Yoga Lover",
-              "Philosopher",
-            ],
-            autoStart: true,
-            loop: true,
-            deleteSpeed: 10,
-          }}
-        />
-      </h2>
+        <h1>Tatia Burdett</h1>
+        <h2>
+          <Typewriter
+            options={{
+              strings: [
+                "Full Stack Web Developer",
+                "Musician",
+                "Photographer",
+                "Traveler",
+                "Mother",
+                "Yoga Lover",
+                "Philosopher",
+              ],
+              autoStart: true,
+              loop: true,
+              deleteSpeed: 10,
+            }}
+          />
+        </h2>
+    </Container>
     </Grid>
   );
 }
