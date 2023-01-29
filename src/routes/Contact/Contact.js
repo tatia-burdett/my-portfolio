db56b20f3738bea9b1e8b97eec706dd51fcd9a8d
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-29 08:28:42 -0600

    contact page sizing

--- src/routes/Contact/Contact.js
+++ src/routes/Contact/Contact.js
@@ -9,19 +9,21 @@ export default function Contact() {
     <Container className='contact' id='contact'>
       <h1>Contact Me</h1>
       <hr />
-      <Row className='contact-section'>
-        <Form action="https://formspree.io/f/xzbkaken " method="POST">
-          <Form.Label>Name:</Form.Label>
-          <Form.Control type="text" placeholder="Name" />
+      <Row className="justify-content-md-center">
+        <Col xs lg="6">
+          <Form action="https://formspree.io/f/xzbkaken " method="POST">
+            <Form.Label>Name:</Form.Label>
+            <Form.Control type="text" placeholder="Name" />
 
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
 
-          <Form.Label>Message</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+            <Form.Label>Message</Form.Label>
+            <Form.Control as="textarea" rows={3} />
 
-          <Button variant='dark' type='submit'>Submit</Button>
-        </Form>
+            <Button variant='dark' type='submit'>Submit</Button>
+          </Form>
+        </Col>
       </Row>
     </Container> 
   )
