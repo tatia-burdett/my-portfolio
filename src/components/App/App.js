963c28958cef076052766fea644f5ac36dcd03cf
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-02-05 14:36:27 -0600

    refactor tabs to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,98 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Row, Col } from 'react-bootstrap';
-import Container from '@mui/material/Container';
+import { Routes, Route, Link } from "react-router-dom";
+import { Grid, Container, Tabs, Tab } from "@mui/material";
+import { ThemeProvider } from "@mui/system";
+import { createTheme } from "@mui/material/styles";
+
 import "./App.css";
 import Home from "../../routes/Home/Home";
-import Contact from '../../routes/Contact/Contact';
-import NavBar from "../NavBar/NavBar";
+import Contact from "../../routes/Contact/Contact";
 import About from "../../routes/About/About";
 import Projects from "../../routes/Projects/Projects";
 import LodgeLog from "../../routes/Projects/LodgeLog/LodgeLog";
 import HelloWorld from "../../routes/Projects/HelloWorld/HelloWorld";
 import SimplyWeather from "../../routes/Projects/SimplyWeather/SimplyWeather";
 
-// move tabs menu here 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#E0FF4F",
+    },
+    secondary: {
+      main: "#11cb5f",
+    },
+  },
+});
+
 function App() {
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className='app'>
-      {/* <NavBar /> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="about" element={<About />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="projects/lodgelog" element={<LodgeLog />} />
-        <Route path="projects/helloworld" element={<HelloWorld />} />
-        <Route path="projects/simplyweather" element={<SimplyWeather />} />
-      </Routes>
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          height="100vh"
+        >
+          <Container
+            sx={{
+              borderRight: 1,
+              borderTop: 1,
+              p: 20,
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            {/* <NavBar /> */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="about" element={<About />} />
+              <Route path="projects" element={<Projects />} />
+              <Route path="projects/lodgelog" element={<LodgeLog />} />
+              <Route path="projects/helloworld" element={<HelloWorld />} />
+              <Route
+                path="projects/simplyweather"
+                element={<SimplyWeather />}
+              />
+            </Routes>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="Test"
+              textColor="primary"
+              orientation="vertical"
+            >
+              <Tab
+                value="1"
+                label="LodgeLog"
+                component={Link}
+                to="/projects/lodgelog"
+              />
+              <Tab
+                value="2"
+                label="Hello World"
+                component={Link}
+                to="/projects/helloworld"
+              />
+              <Tab
+                value="3"
+                label="Simply Weather"
+                component={Link}
+                to="/projects/simplyweather"
+              />
+            </Tabs>
+          </Container>
+        </Grid>
+      </ThemeProvider>
     </div>
   );
 }
--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,87 +1,29 @@
 import React from "react";
 import "./Home.css";
 import Typewriter from "typewriter-effect";
-import { Grid, Container } from "@mui/material";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import { ThemeProvider } from "@mui/system";
-import { createTheme } from "@mui/material/styles";
-import { LinkContainer } from "react-router-bootstrap";
-import { Link, useNavigation } from 'react-router-dom';
 
 export default function Home() {
-  const [value, setValue] = React.useState("1");
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#E0FF4F",
-      },
-      secondary: {
-        main: "#11cb5f",
-      },
-    },
-  });
-
   return (
-    <ThemeProvider theme={theme}>
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        height="100vh"
-      >
-        <Container
-          sx={{
-            borderRight: 1,
-            borderTop: 1,
-            p: 20,
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
+    <div>
+      <p className="header-p">Hello, my name is</p>
+      <h1>Tatia Burdett</h1>
+      <h2>
+        <Typewriter
+          options={{
+            strings: [
+              "Full Stack Web Developer",
+              "JavaScript",
+              "React",
+              "Django",
+              "Python",
+              "SQL",
+            ],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 10,
           }}
-        >
-          <Grid item>
-            <p className="header-p">Hello, my name is</p>
-            <h1>Tatia Burdett</h1>
-            <h2>
-              <Typewriter
-                options={{
-                  strings: [
-                    "Full Stack Web Developer",
-                    "JavaScript",
-                    "React",
-                    "Django",
-                    "Python",
-                    "SQL",
-                  ],
-                  autoStart: true,
-                  loop: true,
-                  deleteSpeed: 10,
-                }}
-              />
-            </h2>
-          </Grid>
-          <Grid item>
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              aria-label="Test"
-              textColor="primary"
-              orientation="vertical"
-            >
-              <Tab value="1" label="LodgeLog" component={Link} to="/projects/lodgelog"/>
-              <Tab value="2" label="Hello World" component={Link} to="/projects/helloworld"/>
-              <Tab value="3" label="Simply Weather" component={Link} to="/projects/simplyweather"/>
-            </Tabs>
-          </Grid>
-        </Container>
-      </Grid>
-    </ThemeProvider>
+        />
+      </h2>
+    </div>
   );
 }
